3ea0458dc8ee02899a5632441ca487614b5f9198
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2022-04-28 11:25:06 -0300

    adicionando spring security

---
+++ src/main/java/com/jacyirice/pw2/aula11/controller/LoginController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jacyirice.pw2.aula11.controller;
+
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author compo
+ */
+@Transactional
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String form() {
+        return "/authentication/login";
+    }
+}
