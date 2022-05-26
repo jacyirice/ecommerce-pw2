989bac5bfb95f0594fad6957bcf794b4c9d70b5a
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2022-05-26 10:28:39 -0300

    adicionando relacionamento com usuario em cliente

--- src/main/java/com/jacyirice/pw2/ecommerce/models/entity/ClientePF.java
+++ src/main/java/com/jacyirice/pw2/ecommerce/models/entity/ClientePF.java
@@ -6,7 +6,10 @@
 package com.jacyirice.pw2.ecommerce.models.entity;
 
 import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -24,6 +27,18 @@ public class ClientePF extends Cliente implements Serializable {
     private String cpf;
     @NotBlank
     private String nome;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
 
     public String getCpf() {
         return cpf;
